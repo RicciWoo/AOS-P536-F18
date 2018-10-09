d1e2c923b6157604cc0518da2485ec0579aee521
RicciWoo <ricciwoo@gmail.com>
2018-10-09 08:15:56 -0400

    update future_prodcons.c

--- apps/future_prodcons.c
+++ apps/future_prodcons.c
@@ -20,3 +20,52 @@ uint future_cons(future_t *fut) {
     printf("Consumed %d\n", i);
     return OK;
 }
+
+struct qnode *newNode(pid32 pid){
+    struct qnode *new_node = (struct qnode *)getmem(sizeof(struct qnode));
+    new_node->pid = pid;
+    return new_node;
+}
+
+struct qnode *initial_queue(){
+  struct qnode *head;
+  struct qnode *tail;
+  head = newNode((pid32)HPID);
+  tail = newNode((pid32)TPID);
+  head->qprev = NULL;
+  tail->qnext = NULL;
+  head->qnext = tail;
+  tail->qprev = head;
+  return head;
+}
+
+int is_empty(struct qnode* head){
+    struct qnode *temp = head->qnext;
+    if(temp->pid == TPID){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+void fenqueue(struct qnode* head, pid32 pid){
+    struct qnode *curr = newNode(pid);
+    struct qnode *temp = head->qnext;
+    head->qnext = curr;
+    temp->qprev = curr;
+    curr->qnext = temp;
+    curr->qprev = head;
+}
+
+pid32 fdequeue(struct qnode* head){
+    struct qnode *tail = head->qnext;
+    while(tail->pid != TPID){
+        tail = tail->qnext;
+    }
+    struct qnode *temp = tail->qprev;
+    struct qnode *last = temp->qprev;
+    last->qnext = tail;
+    tail->qprev = last;
+    return temp->pid;
+}
