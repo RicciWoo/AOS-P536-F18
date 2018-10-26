75d0a3dc5878d9eecadea7bbe965616157fd11dd
RicciWoo <ricciwoo@gmail.com>
2018-10-26 08:58:05 -0400

    test future shared

--- apps/future_prodcons.c
+++ apps/future_prodcons.c
@@ -61,7 +61,7 @@ void fenqueue(struct queue *q, pid32 pid) {
     if (q->head == NULL) {
         q->head = node;
     }
-    printf("enqueue, pid: %d, tail: %d\n", pid, q->tail);
+    printf("enqueue, pid: %d, head: %d, tail: %d\n", pid, q->head, q->tail);
 }
 
 pid32 fdequeue(struct queue *q) {
@@ -75,6 +75,6 @@ pid32 fdequeue(struct queue *q) {
         q->tail = NULL;
     }
     freeNode(temp);
-    printf("dequeue, pid: %d, head: %d\n", pid, q->head);
+    printf("dequeue, pid: %d, head: %d, tail: %d\n", pid, q->head, q->tail);
     return pid;
 }
