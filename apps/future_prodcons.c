64877dfeafd86076db6797e0dffc57f61f4c95a7
RicciWoo <ricciwoo@gmail.com>
2018-10-26 14:30:38 -0400

    correct error future queue

--- apps/future_prodcons.c
+++ apps/future_prodcons.c
@@ -7,18 +7,18 @@
 uint future_prod(future_t *fut, int n) {
     int status;
     status = future_set(fut, n);
+    printf("Produced %d\n", n);
     if (status == SYSERR) {
         printf("future_set failed\n");
         return SYSERR;
     }
-    printf("Produced %d\n", n);
     return OK;
 }
 
 uint future_cons(future_t *fut) {
     int i, status;
     status = (int)future_get(fut, &i);
-    if (status < 1) {
+    if (status == SYSERR) {
         printf("future_get failed\n");
         return SYSERR;
     }
