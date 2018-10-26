e1436dcdea964492e842d40e3701352b86956f4c
RicciWoo <ricciwoo@gmail.com>
2018-10-26 11:28:25 -0400

    test future shared

--- apps/future_prodcons.c
+++ apps/future_prodcons.c
@@ -6,17 +6,13 @@
 
 uint future_prod(future_t *fut, int n) {
     printf("Produced %d\n", n);
-    printf("before future_set, tail: %d\n", fut->get_queue->tail);
     future_set(fut, n);
-    printf("after future_set, tail: %d\n", fut->get_queue->tail);
     return OK;
 }
 
 uint future_cons(future_t *fut) {
     int i, status;
-    printf("before future_get, tail: %d\n", fut->get_queue->tail);
     status = (int)future_get(fut, &i);
-    printf("after future_get, tail: %d\n", fut->get_queue->tail);
     if (status < 1) {
         printf("future_get failed\n");
         return -1;
