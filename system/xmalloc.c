4df1be8ebfd3a4cb4adc59ad6db2915643b52503
RicciWoo <ricciwoo@gmail.com>
2018-10-31 12:22:35 -0400

    assigning buffer sizes

--- system/xmalloc.c
+++ system/xmalloc.c
@@ -3,17 +3,28 @@
 
 void xmalloc_init() {
 	printf("start of void xmalloc_init()\n");
-	//intmask mask = disable();
+	int status = bufinit();
+	if (status == SYSERR) {
+		printf("bufinit failed\n");
+		return;
+	}
+	int poolnum = 11;
+	int minsize = 8;
+	int bufsize[11];
+	for (int i = 0; i < poolnum; i++) {
+		bufsize[i] = minsize << i;
+		printf("bufsize #%d: %d/n", i, bufsize[i]);
+	}
 	bpid32 poolid = mkbufpool(32, 10);
 	//restore(mask);
 	printf("ID of pool that is created: %d\n", poolid);
-	printf("end of void xmalloc_init()\n");
+	printf("end of void xmalloc_init()\n\n");
 }
 
 void *xmalloc(uint32 size) {
 	printf("start of void *xmalloc(int)\n");
 	return NULL;
-	printf("end of void *xmalloc(int)\n");
+	printf("end of void *xmalloc(int)\n\n");
 }
 
 void xfree(void *ptr) {
