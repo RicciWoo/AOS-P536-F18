744bbf3227499daa6ac4332fb4af3d99a0a39eaf
RicciWoo <ricciwoo@gmail.com>
2018-11-01 15:40:13 -0400

    free buffer

--- include/xmalloc.h
+++ include/xmalloc.h
@@ -4,6 +4,9 @@
 extern int32 bufsize[];
 extern int32 bufnumb[];
 
+extern void srand(unsigned long);
+extern unsigned long rand(void);
+
 // declaration of functions
 void xmalloc_init();
 void *xmalloc(uint32);
