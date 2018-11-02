b5e4de4a6ea9a325a31f1c8f697330e6d1cbc3bf
RicciWoo <ricciwoo@gmail.com>
2018-11-02 11:14:09 -0400

    xheap_snapshot()

--- include/xmalloc.h
+++ include/xmalloc.h
@@ -1,20 +1,20 @@
 #ifndef _XMALLOC_H_
 #define _XMALLOC_H_
 
-extern uint32 bufsize[];
-extern uint32 bufnumb[];
-extern uint32 allocBy[];
-extern uint32 allocBf[];
+extern int32 bufsize[];
+extern int32 bufnumb[];
+extern int32 allocBy[];
+extern int32 allocBf[];
 
 extern void srand(unsigned long);
 extern unsigned long rand(void);
 
 // declaration of functions
 void xmalloc_init();
-void *xmalloc(uint32);
+void *xmalloc(int32);
 void xfree(void *);
 char *xheap_snapshot();
-bpid32 findClosestIndex(uint32);
+bpid32 findClosestIndex(int32);
 
 void xmalloc_test();
 
