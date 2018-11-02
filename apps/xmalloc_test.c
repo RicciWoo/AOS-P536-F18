929085cbcc4e15b20113f64fe50f6567aa04fd97
RicciWoo <ricciwoo@gmail.com>
2018-11-02 14:28:35 -0400

    allocate additional space

--- apps/xmalloc_test.c
+++ apps/xmalloc_test.c
@@ -2,17 +2,16 @@
 #include <xmalloc.h>
 
 void xmalloc_test() {
-	// printf("start testing the xmalloc...\n");
-
 	// initialize segregated memory allocation
 	xmalloc_init();
 	
 	// allocate ramdon size of buffers
 	srand(97);
 	int32 ntest = 64 + 32 + 16;
+	// for holding all allocated addresses
 	char **bufptr = (char **)getmem(ntest * sizeof(char *));
 	int32 i;
-	printf("===================== Allocation =====================\n    ");
+	printf("========================= Allocation =========================\n    ");
 	for (i = 0; i < ntest; i++) {
 		int32 bpmaxb;
 		if (i < 64) {
@@ -23,6 +22,7 @@ void xmalloc_test() {
 			bpmaxb = BP_MAXB - sizeof(int32);
 		}
 		int32 size = (int32)(rand() + BP_MINB) % bpmaxb;
+
 		// allocate the buffer
 		char *addr = (char *)xmalloc(size);
 		if (addr == NULL) {
@@ -33,24 +33,37 @@ void xmalloc_test() {
 				printf("\n    ");
 			}
 		}
+
+		// save the address
 		*(bufptr + sizeof(char *) * i) = addr;
 	}
 	printf("\n");
 
+	// show fragmentation information after allocation
+	char *str = xheap_snapshot();
+	printf("========= Fragmentation information after allocation =========\n");
+	printf("%s", str);
+
+	// randomly choose a buffer, then free it
+	// show fragmentation information every 8 steps
+	printf("========================= Random Free ========================\n");
 	for (i = 0; i < ntest; i++) {
-		printf("=====================================================\n");
-		printf("free #%d, address: %d\n", i, *(bufptr + sizeof(char *) * i));
-		char *ptr = *(bufptr + sizeof(char *) * i);
+		// randomly choose one buffer
+		int32 index = (int32)(rand()) % ntest;
+		char *ptr = *(bufptr + sizeof(char *) * index);
 		if (ptr == NULL) {
-			printf("buffer did not allocated!\n");
+			printf("    The buffer was not allocated!!!!\n");
 			continue;
 		}
-		xfree((void *)ptr);
-	}
 
-	char *str = xheap_snapshot();
-	printf("Information about the fragmentation in heap: \n");
-	printf("%s", str);
-	
-	// printf("this is the end of testing.\n");
+		// free the selected buffer
+		xfree((void *)ptr);
+		printf("    free buffer at address: %d\n", i, *(bufptr + sizeof(char *) * index));
+		*(bufptr + sizeof(char *) * index) = NULL;
+
+		// show fragmentation information every 8 steps
+		str = xheap_snapshot();
+		printf("======== Fragmentation information after %d free steps ========\n", i);
+		printf("%s", str);
+	}
 }
