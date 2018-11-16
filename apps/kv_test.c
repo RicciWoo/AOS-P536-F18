00a932dac332073a3542477e46349622c266ad65
RicciWoo <ricciwoo@gmail.com>
2018-11-16 17:43:49 -0500

    setup files

---
+++ apps/kv_test.c
@@ -0,0 +1,27 @@
+#include <xinu.h>
+#include <kv.h>
+#include <xmalloc.h>
+
+
+void kv_test() {
+	// initialize segregated memory allocation
+	xmalloc_init();
+
+	// allocate memory for key and value
+	//int keyLen = strlen(key);
+	//int valLen = strlen(val);
+
+	char *keyPtr;
+	keyPtr = xmalloc(64);
+	memset((void *)keyPtr, 0, 64);
+
+	strncat(keyPtr, "ctttjyhm\0", 9);
+
+	hashFunc(keyPtr);
+	//int hashedVal = hashFunc("rmxrodelucysiaytouifbtryfcdveypsjw");
+
+	printf("hash code for key: %s\n", keyPtr);
+	//printf("hash code for val: %d\n", hashedVal);
+
+	xfree((void *)keyPtr);
+}
--- system/kv_store.c
+++ system/kv_store.c
@@ -1,6 +1,6 @@
 #include <xinu.h>
-#include <xmalloc.h>
 #include <kv.h>
+#include <xmalloc.h>
 
 
 int hashFunc(char *key) {
@@ -69,35 +69,3 @@ char **most_popular_keys(int k) {
 	char *temp = NULL;
 	return &temp;
 }
-
-void kv_test() {
-	// initialize segregated memory allocation
-	xmalloc_init();
-
-	// allocate memory for key and value
-	//int keyLen = strlen(key);
-	//int valLen = strlen(val);
-
-	char *keyPtr;
-	keyPtr = xmalloc(64);
-	memset((void *)keyPtr, 0, 64);
-
-	strncat(keyPtr, "ctttjyhm\0", 9);
-
-	hashFunc(keyPtr);
-	//int hashedVal = hashFunc("rmxrodelucysiaytouifbtryfcdveypsjw");
-
-	printf("hash code for key: %s\n", keyPtr);
-	//printf("hash code for val: %d\n", hashedVal);
-
-	xfree((void *)keyPtr);
-}
-
-
-
-
-
-
-
-
-
