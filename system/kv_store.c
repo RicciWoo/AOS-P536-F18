ccba0b71069200afd44192378d6b0b38bc6f4fe3
RicciWoo <ricciwoo@gmail.com>
2018-11-17 15:48:46 -0500

    create kv_set function

--- system/kv_store.c
+++ system/kv_store.c
@@ -54,6 +54,9 @@ int insertHT(KVNode_t *kvNode) {
 	// get the head of the kv linked list
 	KVNode_t *kvHead = hashTable[hashCode];
 
+	// get length of key
+	int keyLen = strlen(key) + 1; // +1 to hold '\0' at the end
+
 	// check not exist, then insert at the end
 	while (kvHead->next != NULL) {
 		char *keyNode = kvHead->next->keyPtr;
@@ -96,6 +99,7 @@ int kv_set(char *key, char *val) {
 		printf("error inserting node to hash table!\n");
 	}
 
+	int hashCode = hashFunc(keyPtr);
 	KVNode_t *kvHead = hashTable[hashCode];
 	printf("check hash table: \n");
 	while (kvHead->next != NULL) {
