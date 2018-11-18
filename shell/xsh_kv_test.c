56ec6ed9c83ea706897557bdb1e3d18f4e990b32
RicciWoo <ricciwoo@gmail.com>
2018-11-18 18:12:02 -0500

    impl LRU without update value

--- shell/xsh_kv_test.c
+++ shell/xsh_kv_test.c
@@ -4,7 +4,32 @@
 
 shellcmd xsh_kv_test(int nargs, char *args[]) {
 
-	resume( create(trace_small, 1024, 20, "trace_small", 0) );
+	if (nargs >= 3 && strncmp(arg[2], "small", 20) == 0) {
+
+		resume( create(trace_small, 1024, 20, "trace_small", 0) );
+		return 0;
+
+	} else if (nargs >= 3 && strncmp(arg[2], "big", 20) == 0) {
+
+		resume( create(trace_big, 1024, 20, "trace_big", 0) );
+		return 0;
+
+	} else if (nargs >= 3 && strncmp(arg[2], "long", 20) == 0) {
+
+		resume( create(trace_long, 1024, 20, "trace_long", 0) );
+		return 0;
+
+	} else if (nargs >= 3 && strncmp(arg[2], "large", 20) == 0) {
+
+		resume( create(trace_large, 1024, 20, "trace_large", 0) );
+		return 0;
+
+	} else if (nargs >= 3 && strncmp(arg[2], "large", 20) == 0) {
+
+		resume( create(trace_xlarge, 1024, 20, "trace_xlarge", 0) );
+		return 0;
+
+	}
 
 	return 1;
 }
